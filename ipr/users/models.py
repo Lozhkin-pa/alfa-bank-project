525de60662333faf1f8893e11743304de1815127
Spitsyn Maksim <mgraf@yandex.ru>
2024-01-21 11:43:21 +0300

    add model User

--- ipr/users/models.py
+++ ipr/users/models.py
@@ -3,4 +3,10 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    pass
+    photo = models.ImageField(
+        upload_to="users/%Y/%m/%d/", blank=True, null=True, verbose_name="Фото"
+    )
+    patronymic = models.CharField(max_length=200, verbose_name="Отчество")
+    position = models.CharField(max_length=500, verbose_name="Должность")
+    superior = models.BooleanField(default=False, verbose_name="Руководитель")
+    subordinates = models.BooleanField(default=True, verbose_name="Рядовой сотрудник")
