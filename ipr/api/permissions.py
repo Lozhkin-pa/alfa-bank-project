2f6ad1fea01b4ebeea0d26952ae36a26bcad23d9
Spitsyn Maksim <mgraf@yandex.ru>
2024-01-21 15:45:58 +0300

    добавил serializers.py, permissions.py, views.py для Users

---
+++ ipr/api/permissions.py
@@ -0,0 +1,25 @@
+from rest_framework import permissions
+
+
+class IsAuthenticatedReadOnly(permissions.BasePermission):
+    """
+    The request is authenticated as a user, but it does not have permissions
+    to change the resource.
+    """
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        else:
+            return False
+
+
+class IsAdminOrSelf(permissions.BasePermission):
+    """
+    Only admin or owner can edit.
+    """
+
+    def has_object_permission(self, request, view, obj):
+        if request.user.is_staff:
+            return True
+        return obj == request.user
