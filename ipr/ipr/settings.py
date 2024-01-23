44bbcab343b688d248342aa837b8f8eac78f862f
Lozhkin-pa <wsl-user@LAPTOP-12385HBU>
2024-01-23 19:15:03 +0300

    Add Iprs: models, views, serializers, urls, settings

--- ipr/ipr/settings.py
+++ ipr/ipr/settings.py
@@ -51,6 +51,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'django_filters',
+    'iprs.apps.IprsConfig',
 ]
 
 
@@ -140,3 +143,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 5,
+} 
