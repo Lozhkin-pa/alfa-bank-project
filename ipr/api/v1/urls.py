025749319f04b593e77d4f7ca10750bd54de51ec
AlexStr94 <alex.strunskiy@ya.ru>
2024-02-01 10:49:04 +0300

    update dokerization

--- ipr/api/v1/urls.py
+++ ipr/api/v1/urls.py
@@ -5,8 +5,8 @@ from api import views
 
 
 v1_router = DefaultRouter()
-v1_router.register('iprs/my/', views.MyIprViewSet, basename='my_iprs')
-v1_router.register('iprs/subordinates/', views.IprViewSet, basename='iprs')
+v1_router.register('iprs/my', views.MyIprViewSet, basename='my_iprs')
+v1_router.register('iprs/subordinates', views.IprViewSet, basename='iprs')
 v1_router.register('users', views.UserViewSet, basename='users')
 v1_router.register(
     r'tasks/(?P<task_id>\d+)/comments',
