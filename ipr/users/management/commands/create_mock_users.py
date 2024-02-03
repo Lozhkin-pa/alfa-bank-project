54bbc39e9414fbae73be40b8b712a1ef0c85824e
Spitsyn Maksim <mgraf@yandex.ru>
2024-02-03 19:41:57 +0300

    Добавил разные аватарки моковым юзерам

--- ipr/users/management/commands/create_mock_users.py
+++ ipr/users/management/commands/create_mock_users.py
@@ -20,11 +20,12 @@ class Command(BaseCommand):
 
     def create_superior(self) -> User:
         user = User.objects.create(
-            username='superior',
-            first_name='Кирилл',
-            last_name='Сидоров',
-            patronymic='Иванович',
-            position='Team Lead',
+            username="superior",
+            first_name="Кирилл",
+            last_name="Сидоров",
+            patronymic="Иванович",
+            position="Team Lead",
+            photo="users/superior.jpg",
         )
         user.set_password(PASSWORD)
         user.save()
@@ -33,11 +34,12 @@ class Command(BaseCommand):
     def create_subordinates(self, superior: User) -> None:
         for i in range(1, 10):
             user = User.objects.create(
-                username=f'subordinate{i}',
+                username=f"subordinate{i}",
                 first_name=choice(NAMES),
                 last_name=choice(LAST_NAMES),
                 patronymic=choice(SURNAMES),
-                position='Python developer',
+                position="Python developer",
+                photo=f"users/subordinates_{i}",
             )
             user.superiors.add(superior)
             user.set_password(PASSWORD)
