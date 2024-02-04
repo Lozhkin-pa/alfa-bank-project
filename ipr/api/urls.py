0f3a362e84360db79b1fa6faf610879b88c8edec
AlexStr94 <alex.strunskiy@ya.ru>
2024-02-04 13:03:48 +0300

    first edition

--- ipr/api/urls.py
+++ ipr/api/urls.py
@@ -1,7 +1,27 @@
 from django.urls import include, path
 
+from drf_spectacular.views import (
+    SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
+)
+
+
 app_name = 'api'
 
 urlpatterns = [
-    path('v1/', include('api.v1.urls'))
+    path('v1/', include('api.v1.urls')),
+    path(
+        'v1/schema/',
+        SpectacularAPIView.as_view(api_version='api/v1'),
+        name='schema'
+    ),
+    path(
+        'v1/swagger/',
+        SpectacularSwaggerView.as_view(url_name='api:schema'),
+        name='swagger-ui',
+    ),
+    path(
+        'v1/redoc/',
+        SpectacularRedocView.as_view(url_name='api:schema'),
+        name='redoc',
+    ),
 ]
