998d6bb08e7be0072833b4e308e4f064f28bf772
Lozhkin-pa <wsl-user@LAPTOP-12385HBU>
2024-02-18 23:43:12 +0300

    Update settings

--- ipr/ipr/settings.py
+++ ipr/ipr/settings.py
@@ -39,7 +39,7 @@ ALLOWED_HOSTS = os.getenv(
     default='localhost 127.0.0.1 [::1] testserver',
 ).split(' ')
 
-CSRF_TRUSTED_ORIGINS = ['https://alfahackathon.hopto.org']
+# CSRF_TRUSTED_ORIGINS = ['https://alfahackathon.hopto.org']
 
 AUTH_USER_MODEL = 'users.User'
 
@@ -100,16 +100,24 @@ WSGI_APPLICATION = 'ipr.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('POSTGRES_USER'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT')
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.getenv('DB_NAME'),
+            'USER': os.getenv('POSTGRES_USER'),
+            'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+            'HOST': os.getenv('DB_HOST'),
+            'PORT': os.getenv('DB_PORT')
+        }
+    }
 
 
 # Password validation
